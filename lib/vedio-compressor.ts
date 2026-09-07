338bed1a758f2db0557e49e849405c27a4976a67
devamanaly <alyamanhunzik@gmail.com>
2026-09-08 01:33:02 +0500

    auto select codec

--- lib/vedio-compressor.ts
+++ lib/vedio-compressor.ts
@@ -378,7 +378,7 @@ export const compressVideo = async (
           // codec: 'avc',
 
           // DEBUG TARGET
-          hardwareAcceleration: 'prefer-software',
+          hardwareAcceleration: 'no-preference',
 
           quality: new Quality({
             bitrate: videoBitrate,
